9bf7c97a9d889fd7e78ac4af0f1bf5a51ce180a4
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-10-27 18:04:01 +0700

    Add localStorage

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { fetchGithubUser } from "../api/github"
 import UserCard from "./UserCard"
 import RecentSearches from "./RecentSearches"
@@ -7,7 +7,10 @@ import RecentSearches from "./RecentSearches"
 const UserSearch = () => {
   const [username, setUsername] = useState<string>("")
   const [submittedUsername, setSubmittedUsername] = useState<string>("")
-  const [recentUsers, setRecentUsers] = useState<string[]>([])
+  const [recentUsers, setRecentUsers] = useState<string[]>(() => {
+    const stored = localStorage.getItem("recentUsers")
+    return stored ? JSON.parse(stored) : []
+  })
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users", submittedUsername],
@@ -27,6 +30,10 @@ const UserSearch = () => {
       return updated.slice(0, 5)
     })
   }
+
+  useEffect(() => {
+    localStorage.setItem("recentUsers", JSON.stringify(recentUsers))
+  }, [recentUsers])
 
   return (
     <>
